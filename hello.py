c8d0c7785a296d8c01cb72cb3b68c99ea65131c5
JUCSERahull337 <sushmoybarua27@gmail.com>
2021-12-12 23:19:00 +0600

    master branch commit

---
+++ hello.py
@@ -0,0 +1,7 @@
+hungry= input("Are You Hungry? ")
+if hungry=="yes":
+    print("Eat burger")
+    print("Eat Sandwich")
+    print("Have your lunch complete")
+else:
+    print("Do Your Homework!")
